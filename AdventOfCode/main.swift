e60ca5017eecbca28f2ccdafdb1f1860450315a4
Jacob Farkas <jacobf@rkas.net>
2018-11-30 10:26:37 -0800

    Initial Commit

---
+++ AdventOfCode/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AdventOfCode
+//
+//  Created by Jacob Farkas on 11/30/18.
+//  Copyright © 2018 Farktronix. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
