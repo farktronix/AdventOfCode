1c00d112388927cd9c908c748efed47b182c871f
Jacob Farkas <jacobf@rkas.net>
2018-11-30 21:17:04 -0800

    Starting on 2018

---
+++ AdventOfCode/2018/2018.swift
@@ -0,0 +1,19 @@
+//
+//  2018.swift
+//  AdventOfCode
+//
+//  Created by Jacob Farkas on 11/30/18.
+//  Copyright © 2018 Farktronix. All rights reserved.
+//
+
+import Foundation
+
+class AdventCalendar2018 : AdventCalendar {
+    public init() {
+        var days : [Day] = []
+        
+        days.append(Day1_2018())
+        
+        super.init(year: 2018, days: days)
+    }
+}
--- AdventOfCode/main.swift
+++ AdventOfCode/main.swift
@@ -10,6 +10,10 @@ import Foundation
 
 let config = CalendarConfig(outputResults: true, printTiming: true)
 
-let calendar2017 = AdventCalendar2017()
-calendar2017.config = config
-calendar2017.run()
+//let calendar2017 = AdventCalendar2017()
+//calendar2017.config = config
+//calendar2017.run()
+
+let calendar2018 = AdventCalendar2018()
+calendar2018.config = config
+calendar2018.run()
